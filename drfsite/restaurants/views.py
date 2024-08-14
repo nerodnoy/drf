0d9fffffc2a47ae19a43aa7a43526c7508b83cd3
nerodnoy <thatsforsure@mail.ru>
2024-08-14 12:34:51 +0300

    added extra routes w/ @action

--- drfsite/restaurants/views.py
+++ drfsite/restaurants/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics, viewsets
-from .models import Restaurants
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Restaurants, Category
 from .serializer import RestaurantsSerializer
 
 
@@ -8,6 +11,20 @@ from .serializer import RestaurantsSerializer
 class RestaurantsViewSet(viewsets.ModelViewSet):
     queryset = Restaurants.objects.all()
     serializer_class = RestaurantsSerializer
+
+    # Если нам нужно больше маршрутов добавить
+    # Если detail=False, то выведем все записи
+    @action(methods=['get'], detail=False)
+    # Имя маршрута будет формироваться от имени этого метода ниже
+    def category(self, request):
+        categories = Category.objects.all()
+        return Response({'categories': [c.name for c in categories]})
+
+    # Если detail=True, то выведем только одну запись
+    @action(methods=['get'], detail=True)
+    def categories(self, request, pk=None):
+        categories = Category.objects.get(pk=pk)
+        return Response({'categories': categories.name})
 
 
 # class RestaurantsAPIList(generics.ListCreateAPIView):
