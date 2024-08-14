c60ee0d421237c7a5aec3ac8b6775dd5fb85869d
nerodnoy <thatsforsure@mail.ru>
2024-08-14 13:25:47 +0300

    modified model (added User)

--- drfsite/restaurants/serializer.py
+++ drfsite/restaurants/serializer.py
@@ -3,6 +3,8 @@ from .models import Restaurants
 
 
 class RestaurantsSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Restaurants
         fields = '__all__'
